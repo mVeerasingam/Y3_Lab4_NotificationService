215b028d0917235f01368ce588195f5d9b6b1022
Mark Veerasingam <markveer70@gmail.com>
2023-10-11 23:17:43 +0100

    first commit

---
+++ src/main/java/com/example/y3_lab4_part3_notifserv/Y3Lab4Part3NotifServApplication.java
@@ -0,0 +1,13 @@
+package com.example.y3_lab4_part3_notifserv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Y3Lab4Part3NotifServApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Y3Lab4Part3NotifServApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/y3_lab4_part3_notifserv/Y3Lab4Part3NotifServApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.y3_lab4_part3_notifserv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Y3Lab4Part3NotifServApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
